492e05ea064bf42c4c14a8df8be0eb5481b51055
Ankit Pandey <125592261+Ankitpandey321@users.noreply.github.com>
2025-12-05 23:39:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__, static_url_path='')
 
-@app.route('/')
-def index():
+# Serve index.html
+@app.route("/")
+def home():
     return send_from_directory('.', 'index.html')
 
+# Serve static files (CSS, JS, audio)
+@app.route("/<path:filename>")
+def static_files(filename):
+    return send_from_directory('.', filename)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
